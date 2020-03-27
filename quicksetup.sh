300f913199be4b6e46911a81b4bc2c0b4a84e53f
Nick Garvey <garvey.nick@gmail.com>
2020-03-26 23:38:03 -0700

    Update quicksetup.sh
    Add git setup and check for curl

--- quicksetup.sh
+++ quicksetup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-curl 'https://raw.githubusercontent.com/nickgarvey/dotfiles/master/.tmux.conf' > ~/.tmux.conf
-curl 'https://raw.githubusercontent.com/nickgarvey/dotfiles/master/.vimrc' > ~/.vimrc
-curl 'https://raw.githubusercontent.com/seebi/dircolors-solarized/d160260fe7b9624be39bac9716588bd5913a8c48/dircolors.256dark' > ~/.dircolors
+if ! which curl; then
+  echo "Needs curl"
+  exit 1
+fi
+
+curl -s 'https://raw.githubusercontent.com/nickgarvey/dotfiles/master/.tmux.conf' > ~/.tmux.conf
+curl -s 'https://raw.githubusercontent.com/nickgarvey/dotfiles/master/.vimrc' > ~/.vimrc
+curl -s 'https://raw.githubusercontent.com/seebi/dircolors-solarized/d160260fe7b9624be39bac9716588bd5913a8c48/dircolors.256dark' > ~/.dircolors
 
 sed -i -E 's/^(HIST(FILE)?SIZE=)[0-9]+$/\1/' ~/.bashrc
 echo >> ~/.bashrc
@@ -19,3 +24,8 @@ echo 'eval $(dircolors -b ~/.dircolors)' >> ~/.bashrc
 echo >> ~/.bashrc
 echo "export VISUAL=vim" >> ~/.bashrc
 echo "export EDITOR=vim" >> ~/.bashrc
+
+if which git > /dev/null; then
+  git config --global user.name "Nick Garvey"
+  git config --global user.email "garvey.nick@gmail.com"
+fi
